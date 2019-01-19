80b5ef2556136fba1ed2d8a56ea15fbbaad44575
Huyf1995 <spyssor@gmail.com>
2019-01-19 15:11:29 +0800

    add UnionFind and Test

---
+++ src/UnionFind/UF.java
@@ -0,0 +1,10 @@
+package UnionFind;
+
+public interface UF {
+
+    int getSize();
+
+    boolean isConnected(int p, int q);
+
+    void unionElements(int p, int q);
+}
---
+++ src/UnionFind/UnionFind1.java
@@ -0,0 +1,51 @@
+package UnionFind;
+
+public class UnionFind1 implements UF {
+
+    private int[] id;
+
+    public UnionFind1(int size){
+
+        id = new int[size];
+        for (int i = 0; i < id.length; i ++){
+            id[i] = i;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return id.length;
+    }
+
+    private int find(int p){
+
+        if (p < 0 || p >= id.length){
+            throw new IllegalArgumentException("p is out of bound");
+        }
+
+        return id[p];
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+
+        int pid = find(p);
+        int qid = find(q);
+
+        if (pid == qid){
+            return;
+        }
+
+        for (int i = 0; i < id.length; i ++){
+            if(id[i] == pid){
+                id[i] = qid;
+            }
+        }
+    }
+}
